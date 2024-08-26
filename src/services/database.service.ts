433f46c36d23e63079dcf766b7b59eb6ccc64613
manhxuanpham <manhhoathuan@gmail.com>
2024-08-26 22:37:12 +0700

    update register, config env, jwt

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,23 +1,34 @@
-import {Db, MongoClient} from 'mongodb'
+import { Collection, Db, MongoClient } from 'mongodb'
+import User from '~/models/schemas/User.schema'
+import { envConfig } from '~/constants/config'
+import RefreshToken from '~/models/schemas/refreshtoken.schema'
 
 
 
-const uri = "mongodb+srv://manhhoathuan:manhhoathuan123@myproject.ifuwkkq.mongodb.net/?retryWrites=true&w=majority&appName=MyProject"
+const uri = `mongodb+srv://${envConfig.dbUsername}:${envConfig.dbPassword}@myproject.ifuwkkq.mongodb.net/?retryWrites=true&w=majority&appName=MyProject`
+
 class DatabaseService {
-  private client :MongoClient
-  private db :Db
+  private client: MongoClient
+  private db: Db
   constructor() {
     this.client = new MongoClient(uri)
-    this.db = this.client.db('myproject')
+    this.db = this.client.db(envConfig.dbName)
   }
   async connect() {
     try {
       await this.db.command({ ping: 1 })
-      console.log("connect mongodb success")
+      console.log('connect mongodb success')
     } catch (error) {
       console.log(error)
       throw error
     }
+  }
+  get users():Collection<User> {
+    return this.db.collection('users')
+  }
+
+  get refreshTokens():Collection<RefreshToken> {
+    return this.db.collection('refresh_tokens')
   }
 }
 const databaseService = new DatabaseService()
